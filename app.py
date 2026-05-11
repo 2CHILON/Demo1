5919c80da1796e8c15ae6ff231b5e6dce72990f3
2CHILON <nerdneville@gmail.com>
2026-05-11 12:47:48 +0000

    app.py is now a Flask app

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World! \n First program using codespaces and vcs github "                   
+
+@app.route("/new")
+def new():
+    return "Endpoint number 2"            
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
